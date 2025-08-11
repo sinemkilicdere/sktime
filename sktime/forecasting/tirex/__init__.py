0337d00f0fc9b41aeea6204ee0a6960db6ab5e4f
Sinem Kilicdere <sinemkilicdere@gmail.com>
2025-08-10 23:13:44 -0400

    Add TiRexForecaster interface

---
+++ sktime/forecasting/tirex/__init__.py
@@ -0,0 +1,7 @@
+"""TiRex forecaster that cover the TiRex foundation model for zero shot forecasting."""
+
+# copyright: sktime developers
+
+__all__ = ["TiRexForecaster"]
+
+from sktime.forecasting.tirex._tirex import TiRexForecaster
